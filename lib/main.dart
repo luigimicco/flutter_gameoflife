bfd52ab9449449e47ca3c3595193105f1b9f1888
luigimicco <luigi.micco@gmail.com>
2023-05-17 21:43:15 +0200

    empty scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyGOL());
+  runApp(MyGOL(title: 'Flutter Game of Life'));
 }
 
-class MyGOL extends StatelessWidget {
-  const MyGOL({super.key});
+class MyGOL extends StatefulWidget {
+  final String title;
+  const MyGOL({Key? key, required this.title}) : super(key: key);
 
+  @override
+  State<MyGOL> createState() => _MyGOLState();
+}
+
+class _MyGOLState extends State<MyGOL> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Game of Life',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Container(),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: Container()),
     );
   }
 }
