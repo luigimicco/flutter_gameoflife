34d78514660b10b45efa0eb3892d9589337ea445
luigimicco <luigi.micco@gmail.com>
2023-05-18 22:28:28 +0200

    add animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+import 'logic/gol_logic.dart';
+import 'widgets/gol_board.dart';
+
 void main() {
-  runApp(MyGOL(title: 'Flutter Game of Life'));
+  runApp(const MyGOL(title: 'Flutter Game of Life'));
 }
 
 class MyGOL extends StatefulWidget {
@@ -13,14 +18,77 @@ class MyGOL extends StatefulWidget {
 }
 
 class _MyGOLState extends State<MyGOL> {
+  GameOfLife gameOfLife = GameOfLife();
+  bool gameIsRunning = false;
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    gameOfLife.randomWorld();
+    timer =
+        Timer.periodic(const Duration(milliseconds: 500), (Timer t) => next());
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  void next() {
+    if (gameIsRunning) {
+      setState(() {
+        gameOfLife.nextWorld();
+      });
+    }
+  }
+
+  void playStop() {
+    setState(() {
+      gameIsRunning = !gameIsRunning;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    const double paddingSize = 8;
+    final screenSize = MediaQuery.of(context).size;
+
+    double cellWidth = (screenSize.width - paddingSize * 2) / GameOfLife.size;
+    double cellHeight = (screenSize.height - kToolbarHeight - paddingSize * 2) /
+        GameOfLife.size;
+
+    if (cellWidth > cellHeight) {
+      cellWidth = cellHeight;
+    }
+
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text(widget.title),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(paddingSize),
+          child: Column(
+            children: [
+              GolBoard(
+                gameOfLife: gameOfLife,
+                cellSize: cellWidth,
+              ),
+              Text("Generazione: ${gameOfLife.generation}"),
+              Text("Popolazione: ${gameOfLife.population}"),
+            ],
           ),
-          body: Container()),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: playStop,
+          tooltip: 'Start/Stop',
+          child: gameIsRunning
+              ? const Icon(Icons.pause)
+              : const Icon(Icons.play_arrow),
+        ),
+      ),
     );
   }
 }
